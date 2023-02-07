6275c48bfde8e030842612c24883d4420dd6a2d5
turovaes <turovaes@gmail.com>
2023-02-07 18:37:06 +0100

    Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,24 @@
+﻿
+int ReadNumber(string message)
+{
+    Console.Write(message);
+    int n;
+
+    while (!int.TryParse(Console.ReadLine(), out n))
+    {
+        Console.WriteLine("It's not an int");
+    }
+    return n;
+}
+
+int Exponentiate(int numberA, int numberB)
+{
+    int result = (int)Math.Pow(numberA, numberB);
+    return result;
+}
+
+int numberA = ReadNumber("Number A = ");
+int numberB = ReadNumber("Number B = ");
+int answer = Exponentiate(numberA, numberB);
+
+Console.WriteLine($"Число А в степени В = {answer}");
